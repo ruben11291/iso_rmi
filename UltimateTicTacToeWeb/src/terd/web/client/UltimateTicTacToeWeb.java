1bce2d043b1c818cef62e65b7ff8394bbe487397
javi <j.marchan.loro@gmail.com>
2014-04-29 20:21:48 +0200

    adding angel part

--- UltimateTicTacToeWeb/src/terd/web/client/UltimateTicTacToeWeb.java
+++ UltimateTicTacToeWeb/src/terd/web/client/UltimateTicTacToeWeb.java
@@ -1,8 +1,10 @@
 package terd.web.client;
 
 import terd.web.shared.FieldVerifier;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Document;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
@@ -47,7 +49,28 @@ public class UltimateTicTacToeWeb implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
+		
+		final Button register_button = Button.wrap(Document.get().getElementById("register_button"));
+		
+		class RegisterButtonHandler implements ClickHandler, KeyUpHandler{
+
+			@Override
+			public void onKeyUp(KeyUpEvent event) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onClick(ClickEvent event) {
+				register_button.setVisible(false);
+				
+			}
+			
+		}
+		RegisterButtonHandler handler_register_button = new RegisterButtonHandler();
+		register_button.addClickHandler(handler_register_button);		
+		
+		/*final Button sendButton = new Button("Send");
 		final TextBox nameField = new TextBox();
 		nameField.setText("GWT User");
 		final Label errorLabel = new Label();
@@ -115,79 +138,79 @@ public class UltimateTicTacToeWeb implements EntryPoint {
 		dialogVPanel.add(serverResponseLabel);
 		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
 		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
+		dialogBox.setWidget(dialogVPanel);*/
 
 		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
+//		closeButton.addClickHandler(new ClickHandler() {
+//			public void onClick(ClickEvent event) {
+//				dialogBox.hide();
+//				sendButton.setEnabled(true);
+//				sendButton.setFocus(true);
+//			}
+//		});
 
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer,
-						new AsyncCallback<String>() {
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-										.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-
-							public void onSuccess(String result) {
-								dialogBox.setText("Remote Procedure Call");
-								serverResponseLabel
-										.removeStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(result);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-						});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+//		// Create a handler for the sendButton and nameField
+//		class MyHandler implements ClickHandler, KeyUpHandler {
+//			/**
+//			 * Fired when the user clicks on the sendButton.
+//			 */
+//			public void onClick(ClickEvent event) {
+//				sendNameToServer();
+//			}
+//
+//			/**
+//			 * Fired when the user types in the nameField.
+//			 */
+//			public void onKeyUp(KeyUpEvent event) {
+//				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+//					sendNameToServer();
+//				}
+//			}
+//
+//			/**
+//			 * Send the name from the nameField to the server and wait for a response.
+//			 */
+//			private void sendNameToServer() {
+//				// First, we validate the input.
+//				errorLabel.setText("");
+//				String textToServer = nameField.getText();
+//				if (!FieldVerifier.isValidName(textToServer)) {
+//					errorLabel.setText("Please enter at least four characters");
+//					return;
+//				}
+//
+//				// Then, we send the input to the server.
+//				sendButton.setEnabled(false);
+//				textToServerLabel.setText(textToServer);
+//				serverResponseLabel.setText("");
+//				greetingService.greetServer(textToServer,
+//						new AsyncCallback<String>() {
+//							public void onFailure(Throwable caught) {
+//								// Show the RPC error message to the user
+//								dialogBox
+//										.setText("Remote Procedure Call - Failure");
+//								serverResponseLabel
+//										.addStyleName("serverResponseLabelError");
+//								serverResponseLabel.setHTML(SERVER_ERROR);
+//								dialogBox.center();
+//								closeButton.setFocus(true);
+//							}
+//
+//							public void onSuccess(String result) {
+//								dialogBox.setText("Remote Procedure Call");
+//								serverResponseLabel
+//										.removeStyleName("serverResponseLabelError");
+//								serverResponseLabel.setHTML(result);
+//								dialogBox.center();
+//								closeButton.setFocus(true);
+//							}
+//						});
+//			}
+//		}
+//
+//		// Add a handler to send the name to the server
+//		MyHandler handler = new MyHandler();
+//		sendButton.addClickHandler(handler);
+//		nameField.addKeyUpHandler(handler);
 	}
 }
